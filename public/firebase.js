3bb7b4a50526dc1aef35c626ff892405ac67041c
Arpit Kaithwar <arpitkaithwar02@gmail.com>
2023-07-12 00:17:08 +0530

    Intial Commit

---
+++ public/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCAU2tnNNSR8GNIO4pkPhgbk1WNjFEEY40",
+    authDomain: "trendynews-ak123.firebaseapp.com",
+    projectId: "trendynews-ak123",
+    storageBucket: "trendynews-ak123.appspot.com",
+    messagingSenderId: "1046627577199",
+    appId: "1:1046627577199:web:7251f3c4c62e4638c54c9a",
+    measurementId: "G-W5L3ED56ZL"
+};
